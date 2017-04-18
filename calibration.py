767fe7c5a6e017b8c1526aef5932b73767731b31
roger <roger.herikstad@gmail.com>
2017-04-18 14:59:58 +0800

    Fixes a bug where manual calibration was not reset

--- calibration.py
+++ calibration.py
@@ -166,10 +166,9 @@ def calibrate(tracker, reward, cnum=13, paval=1000,target_color=1,
             tracker.send_command("key_function 7 'remote_cal_target 7'")
             tracker.send_command("key_function 8 'remote_cal_target 8'")
             tracker.send_command("key_function 9 'remote_cal_target 9'")
-            #FIXME: Insert is currently not a recognized key
             tracker.send_command("key_function y 'remote_cal_complete'")
         else:
-            tracker.send_command("remote_cal_enable = 1")
+            tracker.send_command("remote_cal_enable = 0")
             tracker.tracker.setAutoCalibrationPacing(paval)
         # Execute custom calibration display
         print '*' * 150
